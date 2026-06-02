835835ef380c8023f5b779b8cbdf168d52f9ac9c
albinbiju <albinbiju532@gmail.com>
2026-06-02 10:07:44 +0530

    changed message app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app=Flask(__name__)
+
+model=joblib.load("penguin_rf_model.pkl")
+encoder=joblib.load("species_encoder.pkl")
+
+@app.route("/")
+def home():
+    return render_template("random.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    bill_length=float(request.form["bill_length"])
+    bill_depth=float(request.form["bill_depth"])
+    flipper_length=float(request.form["flipper_length"])
+    body_mass=float(request.form["body_mass"])
+    sex=float(request.form["sex"])
+    island=float(request.form["island"])
+    features=np.array([[bill_length,bill_depth,flipper_length,body_mass,sex,island]])
+    prediction=model.predict(features)
+    species=encoder.inverse_transform(prediction)
+    return render_template("random.html",prediction_text=f"Predicted Penguin Species: {species[0]}")
+
+if __name__ =="__main__":
+    app.run(debug=True)
